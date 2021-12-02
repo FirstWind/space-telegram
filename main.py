74eaa1091950d14ea65b078aa184f2b22d77ddbe
Dmitrii Pisarenko <FirstWind@mail.ru>
2021-12-03 00:50:16 +0900

    Add feature NASA download picture

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from os import path as os_path
 
 
 def get_response(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response
+    except HTTPError:
+        response.ok
 
 
 def create_dir(path):
@@ -20,43 +23,58 @@ def get_extension(url):
 
 
 def download_img(url, path_image):
-    try:
-        filename = path_image + os_path.basename(url)
-        response = get_response(url)
-        with open(filename, 'wb') as file_img:
-            file_img.write(response.content)
-        print(f"картинку {os_path.basename(url)} cкачали успешно")
-    except HTTPError as http_err:
-        print(f'произошла ошибка HTTP: {http_err}')
-        exit()
+    filename = path_image + os_path.basename(url)
+    response = get_response(url)
+    if not os_path.isdir(path_image):
+        create_dir(path_image)
+    if response:
+        try:
+            with open(filename, 'wb') as file_img:
+                file_img.write(response.content)
+            print(f"картинку {os_path.basename(url)} cкачали успешно")
+        except IOError as error:
+            print(f"Ошибка: {error}")
 
 
-def fetch_spacex_last_launch(url, path_image):
-    try:
-        response = get_response(url)
-        for container_img in response.json():
-            urls = container_img["links"]["flickr_images"]
-            if urls:
-                for url in urls:
-                    download_img(url, path_image)
-                exit()
-    except HTTPError as http_err:
-        print(f'произошла ошибка HTTP: {http_err}')
-        exit()
+def fetch_spacex_last_launch(response, path_image):
+    for container_img in response.json():
+        urls_img = container_img["links"]["flickr_images"]
+        if urls_img:
+            for url in urls_img:
+                download_img(url, path_image)
+            exit()
+
+
+def initial_space_x(url, json_filter):
+    response = get_response(url)
+    if response:
+        fetch_spacex_last_launch(response, save_path)
+    else:
+        space_x_query = os_path.split(url)[0] + json_filter
+        response = get_response(space_x_query)
+        if response:
+            fetch_spacex_last_launch(response, save_path)
+
+
+def initial_nasa(url):
+    response = get_response(url)
+    if response:
+        fetch_nasa(response, save_path)
+
+
+def fetch_nasa(response, path_image):
+    for container_img in response.json():
+        if container_img["url"]:
+            download_img(container_img["url"], path_image)
 
 
 if __name__ == "__main__":
-    url_space_x_latest = "https://api.spacexdata.com/v3/launches/latest?pretty=true"
+    space_x_query = "https://api.spacexdata.com/v3/launches/latest?pretty=true"
+    nasa_query = "https://api.nasa.gov/planetary/apod?start_date=2021-11-15&end_date=2021-12-02&api_key=NkcWH3O7Q7oL5bNRKGN21Q5CXJfH7yDcEHR7mWXw"
     json_filter = "?pretty=true&filter=links(flickr_images)"
-    nasa_query = "https://api.nasa.gov/planetary/apod?api_key=NkcWH3O7Q7oL5bNRKGN21Q5CXJfH7yDcEHR7mWXw"
-    save_path = "images/Nasa/"
-    try:
-        # get_response(url_space_x_latest)
-        # fetch_spacex_last_launch(url_space_x_latest, save_path)
-        get_response(nasa_query)
-        fetch_spacex_last_launch(nasa_query, save_path)
-    except HTTPError:
-        url_space_x = os_path.split(url_space_x_latest)[0] + json_filter
-        fetch_spacex_last_launch(url_space_x, save_path)
-        print("Успешно закончили")
+    save_path = "images/NASA/"
+    # initial_space_x(space_x_query, json_filter)
+    initial_nasa(nasa_query)
+
+    print("Закончили")
 
