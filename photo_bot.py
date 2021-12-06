62cd45db80dc9853a2df80ff1fc0773ddf6ce526
Dmitrii Pisarenko <FirstWind@mail.ru>
2021-12-07 00:25:05 +0900

    Add endless work

--- photo_bot.py
+++ photo_bot.py
@@ -1,6 +1,8 @@
+import time
+
 from dotenv import load_dotenv
 from os import environ
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater
 
 from additional import get_random_photo
 
@@ -13,18 +15,20 @@ def send_photo(updater, CHAT_ID):
     print("Фотография отправлена")
 
 
-def main(token, chat_id) -> None:
+def main(token, chat_id, TIME_SLEEP) -> None:
     updater = Updater(token)
     updater.start_polling()
     print("Бот запущен")
-    send_photo(updater, chat_id)
-    updater.idle()
+    while True:
+        send_photo(updater, chat_id)
+        time.sleep(TIME_SLEEP)
 
 
 if __name__ == "__main__":
     load_dotenv()
     TOKEN_TELEGRAM = environ.get('TOKEN_TELEGRAM')
     CHAT_ID = environ.get('CHANNEL_TELEGRAM_ID')
+    TIME_SLEEP = environ.get('TIME_SLEEP')
 
-    main(TOKEN_TELEGRAM, CHAT_ID)
+    main(TOKEN_TELEGRAM, CHAT_ID, TIME_SLEEP)
     send_photo()
